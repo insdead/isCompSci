064aeff5a1357850a71a2c0458899a45041b27f0
Your Name <you@example.com>
2020-08-11 13:25:23 +0600

    v0.1

--- iscompsci/settings.py
+++ iscompsci/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/photos')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     './blog/templates/blog/static'
 ]
-
-STATIC_URL = '/static/'
